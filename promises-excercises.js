fab9ec8efdb3eb17326410eea9b7168ae36510f4
James Logue <logueja@gmail.com>
2018-03-09 15:08:21 -0700

    Add second exercise

--- promises-excercises.js
+++ promises-excercises.js
@@ -15,3 +15,35 @@ testNum(15)
 testNum(5)
   .then(result => console.log(result))
   .catch(error => console.log(error))
+
+
+const makeAllCaps = wordsArray => {
+  const capitalized = wordsArray.map(word => {
+    return new Promise((resolve, reject) => {
+      if (typeof word === "string"){
+        resolve(word.toUpperCase())
+      } else {
+        reject(Error("No, the array you passed in contained an element that was not a string!"))
+      }
+    })
+  })
+
+  return Promise.all(capitalized)
+}
+
+const sortWords = wordsArray => {
+  return wordsArray.sort((a, b) => {
+    return a > b
+  })
+}
+
+makeAllCaps(['wowow', 'pants', 'bird'])
+  .then(words => sortWords(words))
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+
+
+makeAllCaps([7, 'pants', 'bird'])
+  .then(words => sortWords(words))
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
